163bde6e501910629d20510a9f508ab2dbc66601
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-12-05 14:24:33 +0300

    commit tree 9a23070f9d0db979dd95a67194546c689b088b84

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,33 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find_by!(id: params[:user_id])
+      items = user.items
+      return  render json: items
+    else
+      items = Item.all
+      return  render json: items, include: :user
+    end
   end
 
+  def create
+    item = Item.create!(item_params)
+    render json: item, status: :created
+  end
+
+  def show
+    item = Item.find_by!(id: params[:id])
+    render json: item
+  end
+
+  private
+  def render_not_found_response
+    render json: { error: "Item not found" }, status: :not_found
+  end
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
+  end  
 end
